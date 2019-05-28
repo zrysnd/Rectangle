0f9274894f6a1a29578d3300ea0bd2b49ea78cf1
zrysnd <zrysnd@johnson-atoll-67.dynamic.rpi.edu>
2019-05-27 23:00:08 -0400

    interface design

---
+++ Rectangle/classes/Rectangle.cs
@@ -0,0 +1,101 @@
+﻿using System;
+namespace Rectangle.classes
+{   
+    // Implemented two classes by accident.. 
+    public class Rectangle : IRectangle
+    {
+        private I3dPoint _startPoint;
+        private I3dVector _vector1;
+        private I3dVector _vector2;
+
+        public Rectangle()
+        {
+        }
+         
+        public I3dPoint getPosition()
+        {
+            return _startPoint;
+        }
+
+        public I3dVector getVector1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public I3dVector getVector2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetVectors()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PointIn3D : I3dPoint
+    {
+        private float _x;
+        private float _y;
+        private float _z;
+
+        public PointIn3D()
+        {
+        }
+
+        public void setPositon(float x, float y, float z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+
+        public float GetX()
+        {
+            return _x;
+        }
+        public float GetY()
+        {
+            return _y;
+        }
+        public float GetZ()
+        {
+            return _z;
+        }
+    }
+
+    public class VectorIn3D : I3dVector
+    {
+        private float _compenentX;
+        private float _compenentY;
+        private float _compenentZ;
+
+        public void SetVector(float x, float y, float z)
+        {
+            _compenentX = x;
+            _compenentY = y;
+            _compenentZ = z;
+        }
+
+        public float getXComponent()
+        {
+            return _compenentX;
+        }
+        public float getYComponent()
+        {
+            return _compenentY;
+        }
+        public float getZComponent()
+        {
+            return _compenentZ;
+        }
+
+    }
+
+
+}
---
+++ Rectangle/classes/interface.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Rectangle.classes
+{
+
+
+    public interface I3dPoint
+    {   
+        float GetX();
+        float GetY();
+        float GetZ();
+    }
+
+    public interface I3dVector
+    {
+        float getXComponent();
+        float getYComponent();
+        float getZComponent();
+    }
+
+    public interface IRectangleAccessable
+    {
+        I3dPoint getPosition();
+        I3dVector getVector1();
+        I3dVector getVector2();
+    }
+
+    public interface IRectangleChangable
+    {
+        void SetPosition();
+        void SetVectors();
+    }
+
+    public interface IRectangle : IRectangleChangable, IRectangleAccessable
+    {
+    }
+}
